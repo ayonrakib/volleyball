b37942614ab04158fe849ced937f34275779d10e
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-05-01 14:30:37 -0500

    authenticate in mariadb

--- back-end/controller/UserController.js
+++ back-end/controller/UserController.js
@@ -1,51 +1,35 @@
 const sequelize = require('../mariadb');
 const User = require("../mariadb-models/User")
-// const { Sequelize, DataTypes } = require('sequelize');
-// const sequelize = new Sequelize('mariadb::memory:');
-// const mariadb = require('../mariadb');
-// const User = require('../models/User')
-// const sequelize = new Sequelize(
-//     'volleyball' ,
-//     'root',
-//     'password',
-//     {
-//       host: "127.0.0.1",
-//       dialect: "mariadb"
-//     }
-//   )
-
-// async function connect(){
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Connection has been established successfully.');
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//     }
-// }
-// connect()
-// const User = sequelize.define('user', {
-//     firstName: DataTypes.STRING,
-//     lastName: DataTypes.STRING,
-//     email: DataTypes.STRING,
-//     password: DataTypes.STRING,
-//     session: DataTypes.STRING,
-//     role: DataTypes.STRING
-//   },
-//   {
-//     timestamps: false,
-//     createdAt: false,
-//     updatedAt: false,
-//   });
-
-  
-// (async () => {
-// await sequelize.sync();
-// // Code here
-// })();
+const crypto = require('crypto');
 
 class UserController{
     constructor(){
 
+    }
+    getSalt(){
+        return crypto.randomBytes(16).toString("hex")
+    }
+    hashPassword (password, salt) {
+        if (salt == ""){
+            const salt = this.getSalt();
+            console.log("salt is: ",salt);
+        }
+        var hashedPassword = crypto.pbkdf2Sync(password,salt,1000,64,"sha512").toString("hex");
+        console.log("password is: ",hashedPassword)
+        return hashedPassword
+    }
+
+    async authenticate(email, password){
+        var hashedPassword = this.hashPassword(password, "")
+        var user = await this.getUser(email);
+        console.log("promiseObjectForUser: ",user)
+        if (user == false){
+            return false
+        }
+        console.log("user password is: ",user.password)
+        if(user.password === hashedPassword){
+            return true
+        }
     }
     async search(){
         const usersPromiseObject = await User.findAll();
@@ -59,95 +43,65 @@ class UserController{
         return users;
         
     }
-    async create(){
+    async create(firstName, lastName, email, password){
+        if((typeof firstName != String) || (typeof lastName != String) || (typeof email != String) || (typeof password != String)){
+            return false
+        }
+        var currentUserSalt = this.getSalt();
+        var hashedPassword = this.hashPassword(password, currentUserSalt);
+        
         try{
             const newUser = await User.create({
-                firstName: "fahmida",
-                lastName: "mahjabin",
-                email: "asd@gmail.com",
-                password: "password",
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: hashedPassword,
+                salt: currentUserSalt,
                 session :"asd"
             });
-            console.log("new user is: ",newUser)
+            console.log("new user is: ",newUser.dataValues)
             return true
         }
         catch(error){
-            console.log(error.errors[0].type)
+            console.log(error)
             if(error.errors[0].type == "unique violation"){
                 return false
             }
         }
     }
-    // async getUserWithId(id){
-    //     const user = await User.findAll({
-    //         where:{
-    //             id : id
-    //         }
-    //     })
-    //     if (user == []){
-    //         return false;
-    //     }
-    //     // console.log("found user: ",user[0].dataValues)
-    //     return user[0].dataValues
-    // }
-    // async getUserWithEmail(email){
-    //     const user = await User.findAll({
-    //         where:{
-    //             email : email
-    //         }
-    //     })
-    //     if (user == []){
-    //         return false;
-    //     }
-    //     // console.log("found user: ",user[0].dataValues)
-    //     return user[0].dataValues
-    // }
-    async getUser(attribute){
-        // console.log(typeof 3)
-        // console.log(typeof "3")
-        if(typeof attribute === "number"){
-            const user = await User.findAll({
-                where:{
-                    id : attribute
-                }
-            })
-            if (user == []){
-                return false;
+    async getUserWithId(id){
+        const user = await User.findAll({
+            where:{
+                id : id
             }
-            // console.log("found user: ",user[0].dataValues)
-            return user[0].dataValues
+        })
+        if (user == []){
+            return false;
         }
-        else{
-            const user = await User.findAll({
-                where:{
-                    email : attribute
-                }
-            })
-            if (user == []){
-                return false;
+        // console.log("found user: ",user[0].dataValues)
+        return user[0].dataValues
+    }
+    async getUserWithEmail(email){
+        const user = await User.findAll({
+            where:{
+                email : email
             }
-            // console.log("found user: ",user[0].dataValues)
-            return user[0].dataValues
+        })
+        if (user == []){
+            return false;
         }
-
+        // console.log("found user: ",user[0].dataValues)
+        return user[0].dataValues
     }
 }
 
 const userController = new UserController();
-// console.log("users are: ",userController.search())
-userController.getUser(3).then(user => console.log("User returned with id: ",user))
-userController.getUser("ayon@gmail.com").then(user => console.log("User returned with email: ",user))
+// console.log("user created: ",userController.create("fahmida","mahjabin","eva@gmail.com","password"))
+// console.log("user created: ",userController.create("rakib","ayon","rakib@gmail.com","password"))
+// console.log("user created: ",userController.create("golam","muktadir","golam@gmail.com","password"))
+// console.log("user created: ",userController.create("golam",3,"golam@gmail.com","password").then(message => console.log(message)))
+console.log("authenticated: ",userController.authenticate("eva@gmail.com","password"))
+// userController.getUser(3).then(user => console.log("User returned with id: ",user))
+// userController.getUser("ayon@gmail.com").then(user => console.log("User returned with email: ",user))
+// userController.authenticate("rakib@gmail.com","asd").then(user => console.log("user is: ",user))
 // console.log(userController.create())
-
-// async function search(){
-//     const users = await User.findAll();
-//     return users;
-// }
-// const usersPromiseObject = search();
-// usersPromiseObject.then(data =>{
-//     var users = [];
-//     for(var index=0; index < data.length; index++){
-//         users.push(data[index].dataValues)
-//     }
-//     console.log("users are: ",users)
-// })
